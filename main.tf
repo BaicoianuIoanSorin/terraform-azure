8eb24982d4687e5c3377e45ad8fe0d47cdf30480
nexus <baicoianuioansorin@gmail.com>
2024-09-14 15:21:03 +0200

    init terraform hello world project

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# create a resource group in azure
+# first argument - resource type (azurerm_resource_group), second argument - resource name (mtc-rg)
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "East US"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  # address space for subnet
+  address_space = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
